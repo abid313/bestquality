b992d2083e2a1cba60c8f01efec10a1a51ce60e5
abid313 <abidfajar50@gmail.com>
2022-10-06 23:42:00 +0700

    upload files template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/single-product', function () {
+    return view('single-product');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
