6a3aba8b252037b5c4d9c7442d18a1a4d7cc7067
abid313 <abidfajar50@gmail.com>
2022-10-23 19:55:05 +0700

    balik lagiiii

--- resources/views/ringfushia.blade.php
+++ resources/views/ringfushia.blade.php
@@ -13,8 +13,7 @@
 
 
     <!-- Additional CSS Files -->
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css') }}">
 
@@ -172,8 +171,7 @@ https://templatemo.com/tm-571-hexashop
 
     <!-- Bootstrap -->
     <script src="{{ asset('assets/js/popper.js') }}"></script>
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
     <!-- Plugins -->
     <script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
